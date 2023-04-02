e18f72a832799fa32df4a4521b80a6cd451a766c
Petr Krejcik <p.krejcik@stuart.com>
2023-04-02 10:19:24 +0200

    EP

--- src/routes/api/createSessionCookie/+server.ts
+++ src/routes/api/createSessionCookie/+server.ts
@@ -4,13 +4,19 @@ import {getAuth} from 'firebase-admin/auth'
 export const GET = (async ({ url }) => {
   console.log('🛎 ', 'running server endpoint', url.href);
 
-  const admin = getFirebaseAdmin()
-  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  console.log('🛎 ', 'getting admin');
   try {
-    await getAuth(admin).createSessionCookie('aa', { expiresIn });
-    console.log('🛎 ', 'cookie created');
-  } catch (e) {
-    console.log('🛎 ', 'failed to create a cookie');
+    const admin = getFirebaseAdmin()
+    try {
+      const expiresIn = 60 * 60 * 24 * 5 * 1000;
+      await getAuth(admin).createSessionCookie('aa', { expiresIn });
+      console.log('🛎 ', 'cookie created');
+    } catch (e) {
+      console.log('🛎 ', 'failed to create a cookie');
+    }
+
+  } catch (e) { 
+    console.log('🛎 ', 'cannot create admin', e);
   }
  
   return new Response(String('EP finished'));
