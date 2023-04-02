92c060fbc93dc525f3e9b563dfc4ffae0c0dbfb4
Petr Krejcik <p.krejcik@stuart.com>
2023-04-02 10:54:37 +0200

    fs

--- src/routes/api/fs/+server.ts
+++ src/routes/api/fs/+server.ts
@@ -1,10 +1,14 @@
-import { open } from 'fs';
+import { readFileSync } from 'fs';
 
 export const GET = (async () => {
 
   console.log('🛎 ', 'EP fs');
+
+  const __filename = new URL(import.meta.url).pathname;
+
+  const content = readFileSync(__filename, 'utf8');
   
-  console.log('🛎 ', 'typeof open', typeof open);
-  
+  console.log('🛎 ', 'content', content);
+
   return new Response(String('EP finished'));
 }) ;
